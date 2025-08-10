e4f742923fec3dc656647010b92f40ac7000c4f1
Nabil Zelmat <nabzelmat@gmail.com>
2025-08-10 17:54:21 +0200

    new

---
+++ 8-square.js
@@ -0,0 +1,16 @@
+const size = parseInt(process.argv[2], 10);
+
+if (isNaN(size)) {
+  console.log("Missing size");
+} else if (size > 0) {
+  let row = '';
+  for (let j = 0; j < size; j++) {
+    row += 'X';
+  }
+  for (let i = 0; i < size; i++) {
+    console.log(row);
+  }
+}
+
+
+
